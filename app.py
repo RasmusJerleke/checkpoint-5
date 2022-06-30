492f7e2466c4c36d419867ba782d3dba7f634e19
Nezcobar <hajt@live.se>
2022-06-30 13:49:29 +0200

    action name implemented

--- app.py
+++ app.py
@@ -8,9 +8,15 @@ def read_phonelist(C):
     rows = cur.fetchall()
     cur.close()
     return rows
+def read_name(conn, phone):
+    cur = conn.cursor()
+    cur.execute(f"SELECT name FROM phonelist WHERE phone = '{phone}';")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
 def read_phone(C, name):
     cur = C.cursor()
-    print(f"SELECT phone FROM phonelist WHERE name = '{name}';")
+    # print(f"SELECT phone FROM phonelist WHERE name = '{name}';")
     cur.execute(f"SELECT phone FROM phonelist WHERE name = '{name}';")
     rows = cur.fetchall()
     cur.close()
@@ -78,6 +84,14 @@ def api_func():
         if len(phone) < 1:
             return "not found"
         return phone[0][0]
-    else:
-        return f"Unknown action: '{action}'"
+    if action == 'name':
+        phone = args.get('phone', default="No phone", type=str)
+        if phone == "No phone":
+            return render_template('api_usage.html', action=action)
+        name = read_name(conn, phone)
+        if len(name) < 1:
+            return "not found"
+        return name[0][0]
+    
+    return f"Unknown action: '{action}'"
 
